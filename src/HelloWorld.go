886b362c14090320c86fdd72d4301f1dfec7ac5f
jiaowenqi <jwq0204@163.com>
2021-06-01 15:13:01 +0800

    提交

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var a string = "123"
 	var x, y int = 1, 2
-	/** intVal := 1 相当于 var intVal int = 1 变量已声明未使用会报错
+	/** intVal := 1 相当于 var intVal int = 1 变量已声明未使用会报错 定义常量与全局变量不使用不会报错
 	**/
 	intVal := 1
 	f := "DARK"
@@ -13,6 +13,9 @@ func main() {
 	const ss string = "111"
 	const age = 30
 	const name1, name2 string = "jack", "rusi"
+	const age1, age2 = 14, 23
+	const ff, num = false, 333 //多重赋值，自动推断类型
+	fmt.Println(age1, age2)
 	fmt.Println(name1, name2)
 	fmt.Println(age)
 	fmt.Println(ss)
---
+++ src/Operation.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 15
+	var b int = 20
+	var c int
+	c = a + b
+	fmt.Println(c)
+}
