eb3c9603adfa28702f7a4868159b9e814423e732
jiaowenqi <jwq0204@163.com>
2021-06-29 20:17:53 +0800

    学习

--- src/string-pointer.go
+++ src/string-pointer.go
@@ -9,4 +9,13 @@ func main() {
 	fmt.Printf("Here is the pointer p: %p\n", p)  // prints address
 	fmt.Printf("Here is the string *p: %s\n", *p) // prints string
 	fmt.Printf("Here is the string s: %s\n", s)   // prints same string
+	for i := 0; i < 5; i++ {
+		var v int
+		fmt.Printf("%d ", v)
+		v = 5
+	}
+
+	for i := 0; ; i++ {
+		fmt.Println("Value of i is now:", i)
+	}
 }
